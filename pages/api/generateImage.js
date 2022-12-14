9eb570503f202df44094517f0e49c4803641566b
Pavel Ray <pray@falabella.cl>
2022-12-14 12:17:59 +0530

    Initial Commit for Text to Image app

---
+++ pages/api/generateImage.js
@@ -0,0 +1,19 @@
+import { Configuration, OpenAIApi } from "openai";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { prompt, size } = req.body;
+    const configuration = new Configuration({
+      apiKey: process.env.NEXT_PUBLIC_OPEN_AI_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createImage({
+      prompt,
+      n: 2,
+      size,
+    });
+    res.status(200).json(response.data.data);
+  } else {
+    res.status(200).json({ name: "John Doe" });
+  }  
+}
---
+++ utils/constants.js
@@ -0,0 +1,5 @@
+export const IMAGE_SIZE = {
+    SMALL: '256x256',
+    MEDIUM: '512x512',
+    LARGE: '1024x1024'
+}
